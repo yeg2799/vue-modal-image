a643bd41e97add235805e73b1e2595bdba22b6f0
yeg2799 <yeg2799@gmail.com>
2022-11-10 20:43:12 +0300

    added ext, source and description props

---
+++ src/components/v-modal-images.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .v-modal-images
+    img.v-modal-images-img(:src="ext + source" @click="isOpen = !isOpen" alt="deneme")
+    span {{ description }}
+    #modal(v-if="isOpen")
+      span Modal
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    name: "VModalImages",
+    props: {
+        ext: {
+            type: String,
+            default: 'https://i.imgur.com/',
+            required: true,
+        }, 
+        source: {
+            type: String,
+            default: 'V4fWcfi.jpg',
+            required: true,
+        },
+        description: {
+            type: String,
+            default: '',
+        }
+    },
+    setup() {
+        const isOpen = ref(false);
+
+        return {
+        isOpen
+        }
+    }
+}
+</script>
+
+<style src="./v-modal-images.css" lang="css"></style>
