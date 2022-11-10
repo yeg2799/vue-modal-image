db5cc3722f046f69851574ad3440a2fb06382ddf
yeg2799 <yeg2799@gmail.com>
2022-11-10 22:05:45 +0300

    added buttons

--- src/components/v-modal.vue
+++ src/components/v-modal.vue
@@ -1,7 +1,12 @@
 <template lang="pug">
   .v-modal
-    inline-svg.v-modal__close-button(:src="require('@/assets/close.svg')" width="25" height="25" fill="white" @click="closeModal")
-    img.v-modal-item(:src="imgSource" alt="deneme")
+    .v-modal__head
+        inline-svg.v-modal__head-download.v-modal__head-button(:src="require('@/assets/download.svg')" width="25" height="25" fill="white" @click="closeModal")
+        inline-svg.v-modal__head-fullscreen.v-modal__head-button(:src="require('@/assets/fullscreen.svg')" width="25" height="25" fill="white" @click="closeModal")
+        inline-svg.v-modal__head-rotate.v-modal__head-button(:src="require('@/assets/rotate.svg')" width="25" height="25" fill="white" @click="closeModal")
+        inline-svg.v-modal__head-close.v-modal__head-button(:src="require('@/assets/close.svg')" width="25" height="25" fill="white" @click="closeModal")
+    .v-modal__middle
+        img.v-modal-item(:src="imgSource" alt="deneme")
     //- span.v-modal-image__item-description {{ description }}
 </template>
 
