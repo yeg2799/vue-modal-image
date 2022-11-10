afaaf1951f058bef0b39c8d4c11cc1b8078318da
yeg2799 <yeg2799@gmail.com>
2022-11-10 22:53:55 +0300

    added fullScreen function

--- src/components/v-modal.vue
+++ src/components/v-modal.vue
@@ -2,11 +2,11 @@
   .v-modal
     .v-modal__head
         inline-svg.v-modal__head-download.v-modal__head-button(:src="require('@/assets/download.svg')" width="25" height="25" fill="white" @click="downloadImage(imgSource)")
-        inline-svg.v-modal__head-fullscreen.v-modal__head-button(:src="require('@/assets/fullscreen.svg')" width="25" height="25" fill="white" @click="closeModal")
+        inline-svg.v-modal__head-fullscreen.v-modal__head-button(:src="require('@/assets/fullscreen.svg')" width="25" height="25" fill="white" @click="fullScreenImage")
         inline-svg.v-modal__head-rotate.v-modal__head-button(:src="require('@/assets/rotate.svg')" width="25" height="25" fill="white" @click="rotateImage")
         inline-svg.v-modal__head-close.v-modal__head-button(:src="require('@/assets/close.svg')" width="25" height="25" fill="white" @click="closeModal")
     .v-modal__middle
-        img.v-modal-item(:src="imgSource" alt="deneme" :style="{ 'transform': `translate3d(0%, 0%, 0px) rotate(${rotateDeg}deg)` }" :ref="imageRef")
+        img.v-modal-item(:src="imgSource" alt="deneme" :style="{ 'transform': `translate3d(0%, 0%, 0px) rotate(${rotateDeg}deg)` }" ref="imageRef")
     //- span.v-modal-image__item-description {{ description }}
 </template>
 
@@ -25,6 +25,7 @@ export default {
     setup(props, {emit}) {
         const rotateDeg = ref(0);
         const imageRef = ref(null);
+        const isFullScreen = ref(false);
         
         const closeModal = () => {
             emit('closeModal')
@@ -47,12 +48,32 @@ export default {
             document.body.removeChild(link)
         }
 
+        const fullScreenImage = () => {
+            const image = imageRef.value.style;
+            
+            if (isFullScreen.value) {
+                image.width = "50%";
+                image.height = "auto";
+                image.top = "25%";
+                image.left = "25%";
+                isFullScreen.value = false;
+            } else {
+                image.width = "100%";
+                image.height = "100%";
+                image.top = "auto";
+                image.left = "0%";
+                isFullScreen.value = true;
+            }
+
+        }
+
         return {
             closeModal,
             rotateDeg,
             rotateImage,
             imageRef,
-            downloadImage
+            downloadImage,
+            fullScreenImage,
         }
     }
 } 
